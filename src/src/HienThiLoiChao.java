20bc4feea91bf5b20e13f3f12b7818632bc73f1f
khoanvuviefox <khoanvuviefox@gmail.com>
2021-12-08 12:45:06 +0700

    Hiển thị lời chào

---
+++ src/src/HienThiLoiChao.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class HienThiLoiChao {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Mời nhập tên:");
+        String name = sc.nextLine();
+        System.out.println("Xin chào: " + name);
+    }
+}
