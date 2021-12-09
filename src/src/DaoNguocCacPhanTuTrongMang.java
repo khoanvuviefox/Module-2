3314a882b7a8751ba5135d1ba6e4033abda73e39
khoanvuviefox <khoanvuviefox@gmail.com>
2021-12-09 10:00:20 +0700

    [Thực hành] Đảo ngược các phần tử của mảng

---
+++ src/src/DaoNguocCacPhanTuTrongMang.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class DaoNguocCacPhanTuTrongMang {
+    public static void main(String[] args) {
+        int size;
+        int[] array;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.print("Nhập kích cỡ:");
+            size = scanner.nextInt();
+            if (size > 20)
+                System.out.println("Kích cỡ không vượt quá 20");
+        } while (size > 20);
+        array = new int[size];
+        int i = 0;
+        while (i < array.length) {
+            System.out.print("\n" + "Nhập phần tử " + (i + 1) + ": ");
+            array[i] = scanner.nextInt();
+            i++;
+        }
+        System.out.printf("%-20s%s", "Các phần tử trong mảng: ", "");
+        for (int j = 0; j < array.length; j++) {
+            System.out.print(array[j] + "\t");
+        }
+        for (int j = 0; j < array.length / 2; j++) {
+            int temp = array[j];
+            array[j] = array[size - 1 - j];
+            array[size - 1 - j] = temp;
+        }
+        System.out.printf("\n%-20s%s", "Mảng đảo ngược: ", "");
+        for (int j = 0; j < array.length; j++) {
+            System.out.print(array[j] + "\t");
+        }
+    }
+}
