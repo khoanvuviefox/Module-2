dffbada7f3e0d96260b5a673cabe97b5347d80b2
khoanvuviefox <khoanvuviefox@gmail.com>
2021-12-27 14:27:56 +0700

    [Bài tập] Triển khai các phương thức của ArrayList theo thư viện java.util.ArrayList

---
+++ src/ArrayList_LinkedList/LibraryArrayList/Test.java
@@ -0,0 +1,36 @@
+package ArrayList_LinkedList.LibraryArrayList;
+
+public class Test {
+    public static void main(String[] args) {
+        MyList<String> list = new MyList<>();
+        list.add("khoan");
+        list.add("rất");
+        list.add("đẹp");
+        list.add("trai");
+
+        print(list.getData());
+        System.out.println(list.indexOf("không"));
+        System.out.println(list.indexOf("có"));
+        list.remove(1);
+        list.add("rất",1);
+        print(list.getData());
+        MyList<String> clone = list.clone();
+        System.out.println(clone.contains("toàn"));
+        System.out.println(clone.contains("không"));
+        System.out.println(clone.get(3));
+        System.out.println(clone.size());
+        clone.clear();
+        System.out.println(clone.size());
+        print(clone.getData());
+
+    }
+
+    static void print(Object[] list) {
+        for (Object x : list) {
+            if (!(x == null)) {
+                System.out.println((String) x);
+            }
+        }
+        System.out.println("\n");
+    }
+}
